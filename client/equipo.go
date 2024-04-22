2f18d6513814f1517a3394575e60f51ac729b9db
Fernanda Avendaño <fernanda.avendano@sansano.usm.cl>
2024-04-22 10:30:53 -0400

    test

---
+++ client/equipo.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+
+	pb "github.com/dmoyan0/TareasSD/proto/grpc.proto"
+	"google.golang.org/grpc"
+)
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func Equipos(equipoID int, conn *grpc.ClientConn, wg *sync.WaitGroup) {
+	c := pb.NewWishListc(conn)
+
+	//Esperar 10 seg
+	time.Sleep(10 * time.Second)
+	for {
+		//Cantidad aleatoria munición
+		municionAT := randInt(20, 30)
+		municionMP := randInt(10, 15)
+
+		//Solicitud
+		req := &pb.Request{ID: (equipoID), AT: (municionAT), MP: (municionMP)}
+		fmt.Printf("Equipo %d: Solicitando %d AT y %d MP\n", equipoID, municionAT, municionMP)
+		res, err := c.SolicitarM(context.Background(), req)
+		if err != nil {
+			panic("Equipo %d: Error al realizar la solicitud: %v\n", equipoID, err)
+		} else {
+			if res.Success {
+				fmt.Printf("Equipo %d: Aprobada la solicitud! Conquista Exitosa!\n", equipoID)
+				break
+			} else {
+				fmt.Printf(" Equipo %d: Denegada la solicitud, reintentando en 3 segundos...\n", equipoID)
+				time.Sleep(3 * time.Second)
+			}
+		}
+	}
+	wg.Done()
+}
+
+func main() {
+	//Conexión con servidor Tierra
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	//Error conexión
+	if err != nil {
+		panic("cannot connect with server " + err.Error())
+	}
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		equipoID := i
+		go Equipos(equipoID, conn, &wg)
+	}
+	wg.Wait()
+
+}
---
+++ server/tierra.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"time"
+
+	pb "github.com/dmoyan0/TareasSD/proto"
+	"google.golang.org/grpc"
+)
+
+// Definición servidor Tierra
+type server struct {
+	pb.UnimplementedWishListServiceServer     //<-Interfaz servicio gRPC
+	municionAT                            int //Contador Munición Anti-Terrestre
+	municionMP                            int //Contador Munición Pesada
+	maxAT                                 int
+	maxMP                                 int
+}
+
+func (s *server) SolicitarM(ctx context.Context, request *pb.Request) (*pb.Response, error) {
+	fmt.Printf("Recepción de solicitud desde equipo %d, %d AT y %d MP\n", request.ID, request.AT, request.MP)
+
+	// Verificamos munición
+	if s.municionAT >= int(request.AT) && s.municionMP >= int(request.MP) {
+		// Actualizamos contadores
+		s.municionAT -= int(request.AT)
+		s.municionMP -= int(request.MP)
+		// Éxito
+		fmt.Printf("AT EN SISTEMA: %d; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
+		return &pb.Response{Success: true}, nil
+	}
+
+	// No hay suficiente munición
+	fmt.Printf("AT EN SISTEMA: %d; MP EN SISTEMA: %d\n", s.municionAT, s.municionMP)
+	return &pb.Response{Success: false}, nil
+}
+
+func (s *server) generarMunicion() {
+	for {
+		// Generamos munición cada 5 seg
+		time.Sleep(5 * time.Second)
+
+		// Vemos si hay espacio para la municion AT
+		if s.municionAT < s.maxAT {
+			if s.municionAT+10 <= s.maxAT {
+				s.municionAT += 10
+			} else {
+				s.municionAT = s.maxAT
+			}
+		}
+
+		// Vemos si hay espacio para la municion MP
+		if s.municionMP < s.maxMP {
+			if s.municionMP+5 <= s.maxMP {
+				s.municionMP += 5
+			} else {
+				s.municionMP = s.maxMP
+			}
+		}
+	}
+}
+
+func main() {
+	//Creamos conexion TCP escuchando en el puerto 50051
+	listner, err := net.Listen("tcp", ":50051")
+	//Manejo error
+	if err != nil {
+		panic("cannot create tcp connection" + err.Error())
+	}
+
+	serv := grpc.NewServer()
+	server := &server{municionAT: 0, municionMP: 0, maxAT: 50, maxMP: 20} //Municiones parten en 0
+	pb.RegisterWishListServiceServer(serv, &server)
+
+	//Generamos munición
+	go server.generarMunicion()
+
+	//Error
+	if err = serv.Serve(listner); err != nil {
+		panic("cannot initialize the server" + err.Error())
+	}
+}
